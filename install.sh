5afe8a0d234b323f14c475e3876a5ccb1af3ee1f
Ermanno Ferrari <ermanno@ermannoferrari.net>
2021-03-09 13:57:08 +0000

    Basic commands for an Arch Linux install. Modify username, password, locales and other settings to your liking. Set the execute permission with chmod +x install.sh, and run with ./install.sh.

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:yourpassword | chpasswd
+
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+
+useradd -m username
+echo username:yourpassword | chpasswd
+
+echo "username ALL=(ALL) ALL" >> /etc/sudoers.d/username
+
+/bin/echo -e "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
