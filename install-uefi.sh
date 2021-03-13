34265cd1309887b89aaeb3555a1faa5c8e62fe92
Ermanno Ferrari <ermanno@ermannoferrari.net>
2021-03-13 08:15:16 +0000

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -10,9 +10,12 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:yourpassword | chpasswd
+echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -25,11 +28,16 @@ systemctl enable avahi-daemon
 systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
 
-useradd -m username
-echo username:yourpassword | chpasswd
+useradd -m ermanno
+echo ermanno:password | chpasswd
+usermod -aG libvirt ermanno
 
-echo "username ALL=(ALL) ALL" >> /etc/sudoers.d/username
+echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+
 
 /bin/echo -e "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
