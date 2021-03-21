56063bcc756a1db4bc559ba8edcde9a35c7780ec
Ermanno Ferrari <ermanno@ermannoferrari.net>
2021-03-21 07:11:27 +0000

    Update install-mbr.sh

--- install-mbr.sh
+++ install-mbr.sh
@@ -12,6 +12,8 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+
 pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
