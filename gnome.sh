acb90b6bb7ce914d321697dd88ec2517ec13e353
Ermanno Ferrari <ermanno@ermannoferrari.net>
2021-05-10 05:44:52 +0000

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -8,6 +8,7 @@ sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
+# sudo virsh net-autostart default
 
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur/
@@ -20,7 +21,7 @@ makepkg -si --noconfirm
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
-sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks chrome-gnome-shell simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
+sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks chrome-gnome-shell simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts font-bh-ttf ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 sudo flatpak install -y spotify
 sudo flatpak install -y kdenlive
