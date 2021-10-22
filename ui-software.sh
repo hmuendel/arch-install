042ba533bd5991711c4c730b103c4c916edb982a
chmanie <code@chmanie.com>
2021-10-22 12:44:52 +0200

    Add mopidy

--- ui-software.sh
+++ ui-software.sh
@@ -4,8 +4,13 @@
 sudo pacman -S \
     kitty \
     mpv pavucontrol arc-gtk-theme zathura zathura-pdf-mupdf \
-    ncmpcpp mpc \
+    ncmpcpp mpc mopidy \
     neomutt signal-desktop \
     noto-fonts-emoji \
     qutebrowser libpipewire02 python-adblock pdfjs qt5-wayland python-tldextract
+
+yay -S gomuks libspotify
+
+# mopidy plugins
+sudo python3 -m pip install Mopidy-Spotify Mopidy-Subidy Mopidy-MPD
 
